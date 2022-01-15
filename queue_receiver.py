7c1761c774b54217e6442290506f2a55b076ddd6
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2022-01-15 08:57:26 +0100

    queue_receiver fix

--- queue_receiver.py
+++ queue_receiver.py
@@ -5,19 +5,34 @@ import stomp
 
 class MyListener(stomp.ConnectionListener):
     def on_error(self, frame):
-        print('received an error "%s"' % frame.body)
-
+        print('Received an error "%s"' % frame.body)
     def on_message(self, frame):
-        print('received a message "%s"' % frame.body)
+        print('Received a message "%s"' % frame.body)
+    def on_disconnected(self):
+        print('Disconnected')
+        connect_and_subscribe()
+    def on_connected(self, frame):
+        print("Connected")
+
+
+def connect():
+    conn.connect('artemis', 'simetraehcapa', wait=True, keepalive=True, heartbeats=(4000, 4000))
+
+def subscribe():
+    conn.subscribe(destination='/queue/test-anycast', id=1, ack='auto')
+
+def connect_and_subscribe():
+    connect()
+    subscribe()
 
 hosts = [('localhost', 30979)]
 conn = stomp.Connection(host_and_ports=hosts)
 conn.set_listener('', MyListener())
-conn.connect('artemis', 'simetraehcapa', wait=True)
+connect_and_subscribe()
 
-conn.subscribe(destination='/queue/test-anycast', id=1, ack='auto')
 
-print("Listening for messages...")
+
+
 
 
 time.sleep(99999999)
